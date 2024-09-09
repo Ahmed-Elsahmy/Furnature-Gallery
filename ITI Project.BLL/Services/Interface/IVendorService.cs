8a260bf31ddb0683302c0138f7b86c661010fee4
sara yasser <saraelgazaly2004@gmail.com>
2024-09-09 16:03:11 +0300

    created repo

---
+++ ITI Project.BLL/Services/Interface/IVendorService.cs
@@ -0,0 +1,18 @@
+﻿using ITI_Project.DAL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI_Project.BLL.Services.Interface
+{
+    public interface IVendorService
+    {
+        IEnumerable<Vendor> GetAllVendors();
+        Vendor GetVendorById(int id);
+        bool AddVendor(Vendor vendor);
+        bool UpdateVendor(Vendor vendor);
+        bool DeleteVendor(int id);
+    }
+}
