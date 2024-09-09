7d5435a29af5c79a2b37107f0c8bc2f3346fa0f0
sara yasser <saraelgazaly2004@gmail.com>
2024-09-10 00:27:25 +0300

    make all repos and services and views related to order

---
+++ ITI Project.DAL/Repo/Interface/IOrderRepo.cs
@@ -0,0 +1,18 @@
+﻿using ITI_Project.DAL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI_Project.DAL.Repo.Interface
+{
+    public interface IOrderRepo
+    {
+        Order GetOrderById(int id);
+        IEnumerable<Order> GetAllOrders();
+        void AddOrder(Order order);
+        void UpdateOrder(Order order);
+        void DeleteOrder(int id);
+    }
+}
